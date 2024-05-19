ebd46681228e777993c4032a54cc9069ddbeef8c
Tarun Nagar <tarunnagar8191@gmail.com>
2024-05-19 18:27:39 +0530

    xcountries

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FlagCard from "./components/FlagCard.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <FlagCard />
+    </div>
+  );
+}
+
+export default App;
